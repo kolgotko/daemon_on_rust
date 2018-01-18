c2f6b899d072915ec32354990c8c0188530e05e9
Bykov Igor <igorb.dev@websm.io>
2018-01-19 00:25:51 +0300

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,24 @@
-use std::env;
-use std::process::*;
-use std::path::Path;
+extern crate libc;
+
+use libc::sigaction;
+
+struct Daemon {
+
+}
+
+impl Drop for Daemon {
+
+    fn drop(&mut self) {
+
+        println!("!!!");
+    
+    }
+
+}
 
 fn main() {
 
-    let mut args = env::args();
-    let result = args.find(|x| x == "-d" || x == "--daemon");
-
-    match result {
-        Some(_) => daemonise(),
-        None => {}
-    }
-
-    println!("Working!");
-
-    loop { }
+    let daemon = Daemon{};
 
 }
 
-fn daemonise() {
-
-    let file = env::current_exe().unwrap();
-    let file = file.to_str().unwrap();
-    let root = Path::new("/");
-
-    Command::new(file)
-        .stdin(Stdio::null())
-        .stdout(Stdio::null())
-        .stderr(Stdio::null())
-        .current_dir(&root)
-        .spawn()
-        .expect("Failed spawn process");
-
-    exit(1);
-
-}
